99943a3f66a51b210ac8b59ecb3eec93e074e569
Praveen2407 <73053989+Praveen2407@users.noreply.github.com>
2021-10-12 22:46:07 +0530

    Update BubbleSort.cpp
    Updated the code with minimal travers.

--- BubbleSort.cpp
+++ BubbleSort.cpp
@@ -10,7 +10,7 @@ void BubbleSort(int a[], int n){
     int tmp;
     for(int i=0;i<n-1;i++)
     {    
-        for(int j= 0;j<n-1;j++)
+        for(int j= 0;j<n-1-i;j++)
         {    
             if(a[j] > a[j+1])
             {
