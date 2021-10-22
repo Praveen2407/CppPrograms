956354b9b5a3a79341ebf97c5c84ae7c31db7175
Praveen2407 <73053989+Praveen2407@users.noreply.github.com>
2021-10-22 07:52:09 +0530

    Create duplicate_char_in_string.cpp

---
+++ duplicate_char_in_string.cpp
@@ -0,0 +1,34 @@
+/******************************************************************************
+Praveen 
+
+Find out the duplicate char in string
+Input - ABAC
+Output - A -2 B -1 C -1
+*******************************************************************************/
+
+#include <iostream>
+#include <map>
+using namespace std;
+
+int main()
+{
+    string str;
+    cout << "Enter a string\n";
+    cin >> str;
+
+    map<char , int >m;       
+    for(long i=0;i<str.length();i++)
+    {    
+        //m[str[i]]++; one line solution
+      
+        auto result = m.insert(pair<char,int>(str[i], 1));
+            if (result.second == false)
+                result.first->second++;
+    }
+
+    map<char , int >::iterator itr;    
+    for(itr=m.begin();itr!=m.end();itr++)
+        cout<<itr->first<<" - "<<itr->second<<endl;
+
+    return 0;
+}
