8736de9460916cdc130d8896c1c1f2679ea94794
Praveen2407 <73053989+Praveen2407@users.noreply.github.com>
2025-01-27 17:29:37 +0530

    Create AddTwoNumber_LinkedList.cpp
    Add two number

---
+++ AddTwoNumber_LinkedList.cpp
@@ -0,0 +1,46 @@
+/**
+Input: l1 = [2,4,3], l2 = [5,6,4]
+Output: [7,0,8]
+Explanation: 342 + 465 = 807.
+Example 2:
+
+Input: l1 = [0], l2 = [0]
+Output: [0]
+Example 3:
+
+Input: l1 = [9,9,9,9,9,9,9], l2 = [9,9,9,9]
+Output: [8,9,9,9,0,0,0,1]
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode() : val(0), next(nullptr) {}
+ *     ListNode(int x) : val(x), next(nullptr) {}
+ *     ListNode(int x, ListNode *next) : val(x), next(next) {}
+ * };
+ */
+class Solution {
+public:
+    ListNode* addTwoNumbers(ListNode* l1, ListNode* l2) {
+        ListNode *tmp = new ListNode();
+        ListNode *c = tmp; int carry = 0;
+        //ListNode *p = l2;
+        while(l1 != nullptr || l2 != nullptr || carry > 0){
+            int sum = carry;
+            if(l1 != NULL){
+                sum += l1->val;
+                l1 = l1->next;
+            }
+            if(l2 != NULL){
+                sum += l2->val;
+                l2 = l2->next;
+            }
+
+            carry = sum /10;
+            c->next = new ListNode(sum%10);
+            c = c->next;
+          
+        }
+        return tmp->next;
+    }
+};
